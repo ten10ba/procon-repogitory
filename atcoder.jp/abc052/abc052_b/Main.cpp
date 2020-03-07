f2b986b25c941036786cb920dd8146c8c77240dd
tenba <59526400+ten10ba@users.noreply.github.com>
2020-03-07 12:43:33 +0900

    [AC] abc052 abc052_b

---
+++ atcoder.jp/abc052/abc052_b/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+using namespace std;
+int main(){
+    int n,x=0,mx=0;
+    string s;
+    cin >> n >> s;
+    for(int i = 0; i < n; i++){
+       if(s.at(i)=='I') x++;
+       else x--;
+       if(x>mx) mx=x;
+    }
+    cout << mx << endl;
+}
