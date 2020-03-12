b01726ac65af0781eaab6c051b1a973bb627c193
tenba <59526400+ten10ba@users.noreply.github.com>
2020-03-13 07:39:46 +0900

    [AC] abc046 abc046_b

---
+++ atcoder.jp/abc046/abc046_b/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+using namespace std;
+int main(){
+    int n,s;
+    long long sum;
+    cin  >> n >> s;
+    sum = s;
+    if(n>1){
+        for(int i = 1; i < n; i++){
+            sum*=(s-1);
+        }
+    }
+    cout << sum << endl;
+}
