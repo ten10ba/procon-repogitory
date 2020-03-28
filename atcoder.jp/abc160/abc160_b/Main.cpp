5301efe305fa5c35b58e02a8663d4f8c741a6811
tenba <59526400+ten10ba@users.noreply.github.com>
2020-03-28 21:05:37 +0900

    [AC] abc160 abc160_b

---
+++ atcoder.jp/abc160/abc160_b/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+int main(){
+    long long x;
+    cin >> x;
+    long long x500, x5;
+    x500=x/500;
+    x-=x500*500;
+    x5=x/5;
+    cout << (x500*1000)+(x5*5) << endl;
+}
