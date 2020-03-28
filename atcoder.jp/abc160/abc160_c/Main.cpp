5f98722bf255f0650fdd0a173335faba598ac203
tenba <59526400+ten10ba@users.noreply.github.com>
2020-03-28 21:11:26 +0900

    [AC] abc160 abc160_c

---
+++ atcoder.jp/abc160/abc160_c/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+int main(){
+  int k,n;
+  cin >> k >> n;
+  vector<long long> a(n);
+  for(int i=0; i<n; ++i){
+    cin >>a[i];
+  }
+  sort(a.begin(),a.end());
+  long long dis=a[n-1]-a[0];
+  for(int i=1; i<n; i++){
+    if(k+a[i-1]-a[i]<dis){
+      dis=k+a[i-1]-a[i];
+    }
+  }
+  cout << dis << endl;
+}
