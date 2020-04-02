ed0ca975c58178ac5e04c1d5c36c0a7252c820d0
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-02 21:12:57 +0900

    [AC] abc144 abc144_c

---
+++ atcoder.jp/abc144/abc144_c/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+int main(){
+    long long n,ans1,ans2;
+    cin >> n;
+    for(long long i=sqrt(n); i>0; i--){
+        if(n%i==0){
+            ans1=i;
+            break;
+        }
+    }
+    ans2=n/ans1;
+    cout << ans1+ans2-2 << endl;
+}
