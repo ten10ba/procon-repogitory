d63c20276671f07c41b3af8f5c0b3aa9256a9e15
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-12 12:06:53 +0900

    [AC] abc086 abc086_b

---
+++ atcoder.jp/abc086/abc086_b/Main.cpp
@@ -0,0 +1,32 @@
+//(・ω・)
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+using namespace std;
+
+int main(){
+  string a, b;
+  cin >> a >> b;
+  int c=(stoi)(a+b);
+  int d=sqrt(c);
+  if(c%d==0) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
