dfbf2041b571a97cea10499a47d67696dac8de0d
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-12 12:03:42 +0900

    [AC] panasonic2020 panasonic2020_b

---
+++ atcoder.jp/panasonic2020/panasonic2020_b/Main.cpp
@@ -0,0 +1,35 @@
+//(・ω・)
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+using namespace std;
+
+int main(){
+   unsigned long long h,w,a,b,ans;
+   cin >> h >> w;
+   if(h==1 || w==1) ans=1;
+   else{
+      a=(w+1)/2;
+      b=(h+1)/2;
+      ans=a*b+(h-b)*(w-a);
+   }
+   cout << ans << endl;
+}
