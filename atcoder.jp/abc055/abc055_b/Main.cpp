bd7ff69ee9ab2dc71f0895c7a5ac08d20f3b5d72
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-13 21:29:38 +0900

    [AC] abc055 abc055_b

---
+++ atcoder.jp/abc055/abc055_b/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+typedef long long ll;
+typedef string str;
+int main(){
+    int n;
+    ll ans=1;
+    cin >> n;
+    for(long long i=1; i<=n; i++){
+        ans*=i;
+        ans%=1000000007;
+    }
+    cout << ans << endl;
+}
