2c1cd9804f0463ae988882fe21e1b4f4c6dedbe7
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-13 21:05:40 +0900

    [AC] abc120 abc120_b

---
+++ atcoder.jp/abc120/abc120_b/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+typedef long long ll;
+typedef string str;
+int main(){
+    int a,b,k,ans,count=0;
+    cin >> a >> b >> k;
+    for(int i=min(a,b); i>0; i--){
+        if(a%i==0 && b%i==0){
+            count++;
+            if(count==k){
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+    
+}
