e20ce80ad3d7cd40c44dcd03c9eb3a877cdbebd8
tenba <59526400+ten10ba@users.noreply.github.com>
2020-04-25 21:01:07 +0900

    [AC] abc160 abc160_a

---
+++ atcoder.jp/abc160/abc160_a/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // long long64_t, long long*_t
+#include <cstdio> // prlong longf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+using namespace std;
+int main(){
+  string s;
+  cin >> s;
+  if(s[2]==s[3] && s[4]==s[5]) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
