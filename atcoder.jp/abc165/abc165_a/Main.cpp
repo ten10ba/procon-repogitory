eb52105eaa5e4038069cdb101683fa8af4a5d95c
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-02 21:13:57 +0900

    [AC] abc165 abc165_a

---
+++ atcoder.jp/abc165/abc165_a/Main.cpp
@@ -0,0 +1,41 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   int k,a,b;
+   cin >> k >> a >> b;
+   int j,i;
+   j=a/k;
+   i=b/k;
+   bool c=0;
+   for(int I=j; I<=i; I++){
+       if(I*k>=a && I*k<=b) c=1;
+   }
+   if(c) cout << "OK" << endl;
+   else cout << "NG" << endl;
+}
