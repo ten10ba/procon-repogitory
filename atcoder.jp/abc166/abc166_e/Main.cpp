185754456eb59b36af903dee1429277c981d5411
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-06 12:20:15 +0900

    [AC] abc166 abc166_e

---
+++ atcoder.jp/abc166/abc166_e/Main.cpp
@@ -0,0 +1,46 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    vector<long long> ans1(n);
+    vector<long long> ans2(n);
+    long long ans=0;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        ans1[i]=i-a[i];
+        ans2[i]=a[i]+i;
+    }
+    sort(ans2.begin(),ans2.end());
+    for(int i=0; i<n; i++){
+        ans+=upper_bound(ans2.begin(),ans2.end(),ans1[i])-lower_bound(ans2.begin(),ans2.end(),ans1[i]);
+    }
+    cout << ans << endl;
+}
