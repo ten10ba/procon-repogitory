c4277e0094d404d552a4d52c497b48a44eb083f3
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-09 14:53:10 +0900

    [AC] past201912-open past201912_c

---
+++ atcoder.jp/past201912-open/past201912_c/Main.cpp
@@ -0,0 +1,36 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   vector<int> ans(6);
+   for(int i=0; i<6; i++){
+      cin >> ans[i];
+   }
+   sort(ans.begin(),ans.end());
+   cout << ans[3] << endl;
+}
