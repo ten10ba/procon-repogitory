261b1931e39851bfa48dd4cb5108b770caaf3a03
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-09 15:00:56 +0900

    [AC] past201912-open past201912_d

---
+++ atcoder.jp/past201912-open/past201912_d/Main.cpp
@@ -0,0 +1,51 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   int n,COUNT=0,after;
+   cin >> n;
+   vector<int> ans(n+1);
+   for(int i=0; i<n; i++){
+      int a;
+      cin >> a;
+      ans[a]++;
+      if(ans[a]>1){
+         after=a;
+         COUNT++;
+      }
+   }
+   if(COUNT==1){
+      for(int i=1; i<=n; i++){
+         if(ans[i]==0){
+            cout << after << " " << i << endl;
+            break;
+         }
+      }
+   }
+   else cout << "Correct" << endl;
+}
