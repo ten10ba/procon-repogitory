ae7be09b799df5dfdd07b71478a9792f77f8c6d0
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-09 14:46:06 +0900

    [AC] past201912-open past201912_a

---
+++ atcoder.jp/past201912-open/past201912_a/Main.cpp
@@ -0,0 +1,41 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   string s;
+   cin >> s;
+   bool b=1;
+   for(int i=0; i<3; i++){
+      if(s[i]-'a'<26 && s[i]-'a'>=0){
+         b=0;
+         break;
+      }
+   }
+   if(b) cout << stoi(s)*2 << endl;
+   else cout << "error" << endl;
+}
