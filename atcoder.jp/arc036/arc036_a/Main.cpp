fac6328a4e6a857c6ab12ea9fb0dfdbc0b1c3084
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-11 08:00:44 +0900

    [AC] arc036 arc036_a

---
+++ atcoder.jp/arc036/arc036_a/Main.cpp
@@ -0,0 +1,43 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   int n,k;
+   cin >> n >> k;
+   vector<int> t(n);
+   for(int i=0; i<n; i++) cin >> t[i];
+   int ans = -1;
+   for(int i=0; i<n-2; i++){
+      int s=t[i]+t[i+1]+t[i+2];
+      if(s<k){
+         ans=i+3;
+         break;
+      }
+   }
+   cout << ans << endl;
+}
