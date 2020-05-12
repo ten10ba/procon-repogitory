04a7b65f72068baa2cfc42f5cffaf6177589db76
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-13 07:32:58 +0900

    [AC] abc029 abc029_b

---
+++ atcoder.jp/abc029/abc029_b/Main.cpp
@@ -0,0 +1,45 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    int sum=0;
+    vector<string> s(12);
+    for(int i=0; i<12; i++){
+        cin >> s[i];
+        for(int j=0; j<s[i].size(); j++){
+            if(s[i][j]=='r'){
+                sum++;
+                break;
+            }
+        }
+    }
+    cout << sum << endl;
+
+
+
+}
