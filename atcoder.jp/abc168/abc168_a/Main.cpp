4c4ff738623e5a23daf76ba0ef33d0f0b2c3011d
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-17 21:02:18 +0900

    [AC] abc168 abc168_a

---
+++ atcoder.jp/abc168/abc168_a/Main.cpp
@@ -0,0 +1,36 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+   int n;
+    cin >> n;
+    n%=10;
+    if(n==3) cout << "bon" << endl;
+    else if(n==0 || n==1 || n==6 || n==8) cout << "pon" << endl;
+    else cout << "hon" << endl;
+}
