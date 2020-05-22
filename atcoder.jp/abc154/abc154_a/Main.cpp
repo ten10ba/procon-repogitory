70606362e51a5b063db6731d81c00fd5fd222766
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-22 21:03:36 +0900

    [AC] abc154 abc154_a

---
+++ atcoder.jp/abc154/abc154_a/Main.cpp
@@ -0,0 +1,39 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    string s,t,u;
+    int a,b;
+    cin >> s >> t >> a >> b >> u;
+    if(s==u){
+        cout << a-1 << " " << b << endl;
+    }
+    else{
+        cout << a << " " << b-1 << endl;
+    }
+}
