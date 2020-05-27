1a86dbc672fe47cdda88fd4782bec30978fe62d9
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-28 07:59:51 +0900

    [AC] abc148 abc148_e

---
+++ atcoder.jp/abc148/abc148_e/Main.cpp
@@ -0,0 +1,47 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+long long f(long long n){
+    long long ans=0;
+    if(n>=10){
+        n/=10;
+        ans+=n;
+        while(n>=5){
+            n/=5;
+            ans+=n;
+        }
+    }
+    return ans;
+}
+int main(){
+   long long n;
+   cin >> n;
+   if(n%2==1) cout << 0 << endl;
+   else{
+       cout << f(n) << endl;
+   }
+}
