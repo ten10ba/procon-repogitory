9579356179c6a7df9f98827ce05d12e1fa902025
tenba <59526400+ten10ba@users.noreply.github.com>
2020-05-30 07:38:12 +0900

    [AC] abc093 arc094_a

---
+++ atcoder.jp/abc093/arc094_a/Main.cpp
@@ -0,0 +1,41 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    vector<int> a(3);
+    for(int i=0; i<3; i++ ) cin >> a[i];
+    sort(a.begin(),a.end());
+    int ans=0;
+    ans+=a[2]-a[1];
+    if((a[1]-a[0])%2==0){
+        cout << ans+(a[1]-a[0])/2 << endl;
+    }
+    else{
+        cout << ans+(a[1]-a[0])/2+2 << endl;
+    }
+}
