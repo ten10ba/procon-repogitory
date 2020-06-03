cbe07e76195059819370a5d94fe42d3aaca95468
tenba <59526400+ten10ba@users.noreply.github.com>
2020-06-03 17:33:36 +0900

    [AC] arc017 arc017_1

---
+++ atcoder.jp/arc017/arc017_1/Main.cpp
@@ -0,0 +1,44 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    bool ans=true;
+    if(n==1) ans=false;
+    else{
+        for(long long i=2; i*i<=n; i++){
+            if(n%i==0){
+                ans=false;
+                break;
+            }
+        }
+    }
+    if(ans) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
