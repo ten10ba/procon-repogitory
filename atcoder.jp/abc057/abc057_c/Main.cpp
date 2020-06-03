0bcba3329378fdcf4eaf1474c9cff64b047380a7
tenba <59526400+ten10ba@users.noreply.github.com>
2020-06-03 17:45:20 +0900

    [AC] abc057 abc057_c

---
+++ atcoder.jp/abc057/abc057_c/Main.cpp
@@ -0,0 +1,47 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    long long n,ans;
+    cin >> n;
+    long long x=sqrt(n);
+    for(long long i=x; i>=1; i--){
+        if(n%i==0){
+            ans=n/i;
+            break;
+        }
+    }
+    long long x10=1;
+    for(int i=0; i<=10; i++){
+        if(ans/x10==0){
+            cout << i << endl;
+            break;
+        }
+        x10*=10;
+    }
+}
