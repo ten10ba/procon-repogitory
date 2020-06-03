69111a4ddbe487390055f7d79fddc25cadf2c3a5
tenba <59526400+ten10ba@users.noreply.github.com>
2020-06-03 09:08:31 +0900

    [AC] keyence2019 keyence2019_b

---
+++ atcoder.jp/keyence2019/keyence2019_b/Main.cpp
@@ -0,0 +1,42 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    bool ans=0;
+    for(int i=0; i<=7; i++){
+        string a=s.substr(0,i)+s.substr(i+s.size()-7,7-i);
+        if(a=="keyence"){
+            ans=1;
+            break;
+        }
+    }
+    if(ans) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
