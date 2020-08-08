04b971531c079f5b80afa2098ffc479efd1058a0
tenba <59526400+ten10ba@users.noreply.github.com>
2020-08-09 00:53:01 +0900

    [AC] abc155 abc155_a

---
+++ atcoder.jp/abc155/abc155_a/Main.cpp
@@ -0,0 +1,35 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a==b && a!=c) cout << "Yes" << endl;
+    else if(b==c && b!=a) cout << "Yes" << endl;
+    else if(a==c && a!=b) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
