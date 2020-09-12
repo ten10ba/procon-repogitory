aa7a99bf6f30cd5f9181793575ee033a2a7b5708
tenba <59526400+ten10ba@users.noreply.github.com>
2020-09-12 16:41:34 +0900

    [AC] abc132 abc132_c

---
+++ atcoder.jp/abc132/abc132_c/Main.cpp
@@ -0,0 +1,41 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+#include <ctime>
+#include <functional>
+#include <cassert>
+using namespace std;
+const double pi=3.141592653589793238;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+    sort(a.begin(),a.end());
+    cout << a[n/2]-a[n/2-1] << endl;
+}
+
