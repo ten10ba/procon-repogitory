7433b88a4cb5bdba9dff376d2ac8aff710983072
tenba <59526400+ten10ba@users.noreply.github.com>
2020-09-13 21:01:08 +0900

    [AC] abc178 abc178_a

---
+++ atcoder.jp/abc178/abc178_a/Main.cpp
@@ -0,0 +1,39 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+int main(){
+    int x;
+    cin >> x;
+    if(x==1){
+        cout << 0 << endl;
+    }
+    else{
+        cout << 1 << endl;
+    }
+}
+
