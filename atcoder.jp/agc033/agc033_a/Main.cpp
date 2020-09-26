82efb8430f930d813e4337d79986a546e424319d
tenba <59526400+ten10ba@users.noreply.github.com>
2020-09-26 14:30:45 +0900

    [AC] agc033 agc033_a

---
+++ atcoder.jp/agc033/agc033_a/Main.cpp
@@ -0,0 +1,72 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+int main(){
+    int h,w;
+    cin >> h >> w;
+    vector<vector<char>> a(h,vector<char>(w));
+    vector<vector<int>> dist(h,vector<int>(w,-1));
+    int ans=0;//変えた回数
+    queue<pair<int,int>> q;
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){
+            cin >> a[i][j];
+            if(a[i][j]=='#'){
+                dist[i][j]=0;
+                q.push(make_pair(i,j));
+            }
+        }
+    }
+    while(!q.empty()){
+        pair<int,int> p=q.front();
+        q.pop();
+        int ph=p.first,pw=p.second;
+        int dh[4]={-1,0,0,1};
+        int dw[4]={0,-1,1,0};
+        bool sousa=0;
+        for(int i=0; i<4; i++){
+            int H=ph+dh[i],W=pw+dw[i];
+            if(H>=h || W>=w) continue;
+            else if(H<0 || W<0) continue;
+            else if(dist[H][W]!=-1) continue;
+            else{
+                dist[H][W]=dist[ph][pw]+1;
+                q.push(make_pair(H,W));
+            }
+        }
+    }
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){
+            if(dist[i][j]>ans) ans=dist[i][j];
+        }
+    }
+
+    cout << ans << endl;
+
+}
+
