054c46f25e016b8d156fb109c4c107d12a47b61a
tenba <59526400+ten10ba@users.noreply.github.com>
2020-10-10 22:52:29 +0900

    [AC] hhkb2020 hhkb2020_c

---
+++ atcoder.jp/hhkb2020/hhkb2020_c/Main.cpp
@@ -0,0 +1,45 @@
+//(・ω・)
+#define _GLIBCXX_DEBUG
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+int main(){
+    int n;
+    cin >> n;
+    set<int> s;
+    int iter=0;
+    for(int i=0; i<n; i++){
+        int num;
+        cin >> num;
+        s.insert(num);
+        while(s.count(iter)){
+            iter++;
+        }
+        cout << iter << endl;
+    }
+}
+
