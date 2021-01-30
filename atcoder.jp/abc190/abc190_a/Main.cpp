ee6232b2c3bfc097130bd8a2d439fbf1aa5b2da2
tenba <59526400+ten10ba@users.noreply.github.com>
2021-01-30 21:03:50 +0900

    [AC] abc190 abc190_a

---
+++ atcoder.jp/abc190/abc190_a/Main.cpp
@@ -0,0 +1,42 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    bool ans=0;
+    if(c){
+        if(b<=a) ans=1;
+    }
+    else{
+        if(a>b) ans=1;
+    }
+    if(ans) cout << "Takahashi" << endl;
+    else cout << "Aoki" << endl;
+}
