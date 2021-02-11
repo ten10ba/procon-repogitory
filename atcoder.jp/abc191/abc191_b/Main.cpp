0d5f4e8ac745c03a3b3909e066a856275878ae61
tenba <59526400+ten10ba@users.noreply.github.com>
2021-02-12 00:16:17 +0900

    [AC] abc191 abc191_b

---
+++ atcoder.jp/abc191/abc191_b/Main.cpp
@@ -0,0 +1,45 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+
+int main(){
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<long long> a;
+    for(int i = 0; i < n; i++) {
+        long long b;
+        cin >> b;
+        if(b != x) a.push_back(b);
+    }
+    if(a.size() == 0) cout << endl;
+    for(int i = 0; i < a.size(); i++) {
+        cout << a[i] << endl;
+    }
+
+}
