9e08688306e2a7e6c76206353c744028561edcec
tenba <59526400+ten10ba@users.noreply.github.com>
2021-02-11 17:11:57 +0900

    [AC] abc190 abc190_d

---
+++ atcoder.jp/abc190/abc190_d/Main.cpp
@@ -0,0 +1,46 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+const double pi=3.141592653589793238;
+
+int main(){
+    long long n, ans = 0;
+    cin >> n;
+    long long sqrtn = sqrt(n);
+    for(long long i = 1; i <= sqrtn; i++) {
+        if(n % i == 0) {
+            if(i % 2 == 0){
+                if((n / i) % 2 != 0) ans++;
+            }
+            else {
+                ans++;
+                if((n / i) % 2 != 0  && (n / i) != i) ans++;
+            }
+        }
+    }
+    cout << ans*2 << endl;
+}
