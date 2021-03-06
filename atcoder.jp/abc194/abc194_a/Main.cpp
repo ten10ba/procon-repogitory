8b58d37104f8246a43455c257ad618e9dfd00ef8
tenba <59526400+ten10ba@users.noreply.github.com>
2021-03-06 21:03:06 +0900

    [AC] abc194 abc194_a

---
+++ atcoder.jp/abc194/abc194_a/Main.cpp
@@ -0,0 +1,35 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(a+b>=15 && b>=8) cout << 1 << endl;
+    else if (a+b>=10 && b>=3) cout << 2 << endl;
+    else if(a+b>=3) cout << 3 << endl;
+    else cout << 4 << endl;
+}
