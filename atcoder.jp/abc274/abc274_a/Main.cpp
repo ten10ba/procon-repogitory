74387071d1013baec188d3372a3fa9aab9c06587
tenba <59526400+ten10ba@users.noreply.github.com>
2022-10-22 21:10:33 +0900

    [AC] abc274 abc274_a

---
+++ atcoder.jp/abc274/abc274_a/Main.cpp
@@ -0,0 +1,35 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    int s = 1000 * (double)b / a + 0.5;
+    double S = (double)s/1000;
+    cout << fixed;
+    cout << setprecision(3) << S << endl;
+}
