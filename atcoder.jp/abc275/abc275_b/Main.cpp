4b1c55125ece7a55cc3d8e25a6081477e71dec84
tenba <59526400+ten10ba@users.noreply.github.com>
2022-10-29 21:12:13 +0900

    [AC] abc275 abc275_b

---
+++ atcoder.jp/abc275/abc275_b/Main.cpp
@@ -0,0 +1,38 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    long long a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    const long long mod = 998244353;
+    long long ans;
+    ans = ((((a  % mod) * (b % mod)) % mod) * (c %mod) % mod) - ((((d % mod) * (e % mod)) % mod) * (f % mod) % mod);
+    ans %= mod;
+    if(ans < 0) ans += mod;
+    cout << ans << endl;
+}
