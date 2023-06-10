5c530bb461b600c1824fcc7890ba3ecff39f4ad5
tenba <59526400+ten10ba@users.noreply.github.com>
2023-06-10 21:03:10 +0900

    [AC] abc305 abc305_a

---
+++ atcoder.jp/abc305/abc305_a/Main.cpp
@@ -0,0 +1,43 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans;
+    if(n % 5 < 3){
+        ans = n / 5;
+        cout << ans * 5 << endl;
+    }
+    else{
+        ans = n / 5 + 1;
+        cout << ans * 5 << endl;
+    }
+    
+    return 0;
+}
