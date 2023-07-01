bb272b1803a1cb419f0982dfb3dff1df6b8bf439
tenba <59526400+ten10ba@users.noreply.github.com>
2023-07-01 21:04:17 +0900

    [AC] abc308 abc308_a

---
+++ atcoder.jp/abc308/abc308_a/Main.cpp
@@ -0,0 +1,45 @@
+//(・ω・)
+#include <stdio.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, atoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <iomanip> //setprecision
+#include <limits>
+#include <sstream> //istringstream
+#include <stdlib.h>
+#include <list>
+#include <iterator>//std::advance()
+using namespace std;
+
+
+int main(){
+    vector<int> s(8);
+    string ans = "Yes";
+    for(int i = 0; i < 8; ++i){
+        cin >> s[i];
+        if(i != 0){
+            if(s[i] < s[i - 1]) ans = "No";
+        }
+        if(s[i] < 100) ans = "No";
+        if(s[i] > 675) ans = "No";
+        if(s[i] % 25  != 0) ans = "No";
+    }
+    cout << ans << endl;
+
+    return 0;
+}
